2e646bbe68859b0f9ec0b7539be0f9ef9fbef8c5
coacer <coacer728h23@icloud.com>
2019-06-14 16:09:29 +0900

    投稿時にtagを空欄にしたら空のタグが追加されない設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
   has_many :tags, dependent: :destroy
-  accepts_nested_attributes_for :tags, allow_destroy: true
+  accepts_nested_attributes_for :tags, allow_destroy: true, reject_if: :all_blank
 end
